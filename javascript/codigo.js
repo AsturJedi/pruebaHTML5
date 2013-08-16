dc34e109c113b29ab5fe6fab26fd6dce6d8879a5
AsturJedi <asturjedi@gmail.com>
2013-08-16 18:30:14 +0200

    Mensaje al enviar formulario

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -1,6 +1,11 @@
 
 
-function saluda(element)
-			{	element.innerHTML="Qué tal estáis pasándolo?";
-				element.style.fontSize="3em";
-			}
+function saluda(element){	
+	element.innerHTML="Qué tal estáis pasándolo?";
+	element.style.fontSize="3em";
+}
+			
+function pulsado(elemento) {
+ alert(" Formulario Enviado ");
+ 
+}
