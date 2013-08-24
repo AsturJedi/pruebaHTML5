b08efc32cf5a35e8c6f74fe07da3607765bb3334
AsturJedi <asturjedi@gmail.com>
2013-08-24 18:33:56 +0200

    Arreglos en el formulario.

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -3,13 +3,31 @@
 function saluda(element){	
 	element.innerHTML="Bienvenid@s al Restaurante Casa Adela. ";
 	element.style.fontSize="1em";
+		
 }
-				
+
+
 function pulsado(elemento) {
-	
-	alert("\n Reserva Enviada ");
- 
+
+	alert("La Reserva a enviar debe contener al menos los siguientes campos: \n Nombre \n Telf. \n Fecha de la Reserva  \n\n Muchas Gracias");
 }
 
 
+/*
+//$("#resv").click("submit", enviar);
+function enviar(){ //Envío de reserva
 
+	cliente = $("#cliente").val();
+	telf = $("#telf").val();
+	date = $("#fechaReserva").val();
+	
+	if(cliente.length == 0 || telf.length == 0 || date.length == 0 )
+	{
+		alert("Debes rellenar los campos vacíos.");
+	}
+	else 	
+		alert("Reserva:"+"\n\nNombre: "+cliente+"\nCorreo: "+correo+"\nTeléfono: "+telf+"\nAsistentes: "+asistentes+"\nFecha: "+date+"\nComedor"+prefer);
+
+	return false;
+}
+*/
